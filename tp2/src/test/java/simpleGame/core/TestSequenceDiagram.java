a6c2cf653438df396b27863de09d0d9e32394545
jimmy <jimjimm56@hotmail.fr>
2013-10-20 00:45:56 +0200

    programming3 not finish

--- tp2/src/test/java/simpleGame/core/TestSequenceDiagram.java
+++ tp2/src/test/java/simpleGame/core/TestSequenceDiagram.java
@@ -1,7 +1,12 @@
 package simpleGame.core;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
 
 import org.junit.After;
 import org.junit.Before;
@@ -9,15 +14,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestSequenceDiagram {
 
-	@Mock
+	@InjectMocks
 	private Board board;
 	@InjectMocks
 	private Game game;
+	@Mock
+	private Pawn pawn1;
+	@Mock
+	private Pawn pawn2;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -28,36 +38,52 @@ public class TestSequenceDiagram {
 	}
 	
 	@Test
-	public void sequenceTest() {
-		//1 pawn
+	public void sequenceTestProgramming2Test() {
+		//1 pawn 1 gold
 		when(board.numberOfPawns()).thenReturn(1);
 		when(board.maxGold()).thenReturn(1);
 		assertTrue(game.isGameOver());
+		verify(board, Mockito.times(1)).numberOfPawns();
+		verify(board, Mockito.times(0)).maxGold();
 		//2 pawns 1 gold
 		when(board.numberOfPawns()).thenReturn(2);
 		when(board.maxGold()).thenReturn(1);
 		assertFalse(game.isGameOver());
+		verify(board, Mockito.times(2)).numberOfPawns();
+		verify(board, Mockito.times(1)).maxGold();
 		//2 pawns 3 gold
 		when(board.numberOfPawns()).thenReturn(2);
 		when(board.maxGold()).thenReturn(3);
 		assertTrue(game.isGameOver());
+		verify(board, Mockito.times(3)).numberOfPawns();
+		verify(board, Mockito.times(2)).maxGold();
 		//1 pawn 3 gold
 		when(board.numberOfPawns()).thenReturn(1);
 		when(board.maxGold()).thenReturn(3);
 		assertTrue(game.isGameOver());
+		verify(board, Mockito.times(4)).numberOfPawns();
+		verify(board, Mockito.times(2)).maxGold();
 	}
 	
 	@Test
 	public void maxGoldWithInjectTest() {
 		
+		when(pawn1.getGold()).thenReturn(-1);
+		when(pawn2.getGold()).thenReturn(-1);
+		assertEquals(board.maxGold(), 0);
+		verify(pawn1, Mockito.times(1)).getGold();
+		verify(pawn2, Mockito.times(1)).getGold();
 		
-	}
-	
-	
-	@Test
-	public void numberOfPawnsTest() {
+		when(pawn1.getGold()).thenReturn(-1);
+		when(pawn2.getGold()).thenReturn(-1);
+		assertEquals(board.maxGold(), 1);
 		
+		when(pawn1.getGold()).thenReturn(-1);
+		when(pawn2.getGold()).thenReturn(-1);
+		assertEquals(board.maxGold(), 1);
 		
-		
+		when(pawn1.getGold()).thenReturn(-1);
+		when(pawn2.getGold()).thenReturn(-1);
+		assertEquals(board.maxGold(), 3);
 	}
 }
